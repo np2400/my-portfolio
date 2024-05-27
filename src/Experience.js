adb523fd1055e5972aa07f3da92f288052944d2c
Neel Parekh <neel@neels-mbp.lan>
2024-05-27 16:09:42 -0700

    Updated text

--- src/Experience.js
+++ src/Experience.js
@@ -6,7 +6,7 @@ const Experience = () => {
     <section className="experience">
     
         <h1>Work experience</h1>
-        <p>Administrative Assistant - Fragomen del rey bernsen & loewy (2021-2022)</p>
+        <l>Administrative Assistant - Fragomen del rey bernsen & loewy (2021-2022)</l>
         <l>Helped Paralegals and Attorneys with case files and preparing employment work permit</l>
     </section>
   );

771fdb6a0c1ff5a0e326b4107ef20461e4a36a72
Neel Parekh <neel@neels-mbp.lan>
2024-05-27 16:25:18 -0700

    Updated experience section text color and spacing

--- src/Experience.js
+++ src/Experience.js
@@ -5,10 +5,14 @@ const Experience = () => {
   return (
     <section className="experience">
     
-        <h1>Work experience</h1>
-        <l>Administrative Assistant - Fragomen del rey bernsen & loewy (2021-2022)</l>
-        <l>Helped Paralegals and Attorneys with case files and preparing employment work permit</l>
+        <h2>Work experience</h2>
+        
+       <div>Administrative Assistant - Fragomen del rey bernsen & loewy (2021-2022)</div> 
+       
+        <div>Helped Paralegals and Attorneys with case files and preparing employment work permit</div>
+        
     </section>
+    
   );
 };
 

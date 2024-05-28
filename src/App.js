9090013fa6bac817476329d85aa33ffa7f9200de
Neel Parekh <neel@Neels-MacBook-Pro.local>
2024-05-27 21:50:12 -0700

    Add resume PDF and update App.js and Hero.css for Git

--- src/App.js
+++ src/App.js
@@ -17,7 +17,15 @@ function App() {
       <div id="about">
         <section className="about-section">
           <h2>About Me</h2>
-          Software Developer with a passion for creating efficient and scalable solutions.
+          <p>
+          I am a senior at the University of California, Riverside, majoring in Computer Science with Business Applications. I have a keen interest in data analytics. I am currently looking for a job in the field and am a team player who enjoys collaborative environments.
+          I am profiecinet in Javascript, C++ and Python.  I am passionate about leveraging my skills to solve real-world problems and contribute to innovative projects.
+          </p>
+          <p>
+            <a href="/Neels Resume.pdf" target="_blank" rel="noopener noreferrer">
+              View my resume
+            </a>
+          </p>
         </section>
       </div>
       <div id="education">
